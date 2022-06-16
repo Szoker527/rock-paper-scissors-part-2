7390dfd9db9cba495593120f6d4fedb957923d69
szoker527 <szoker527@gmail.com>
2022-06-16 13:39:56 +0200

    Project almost done
    Added only basic structure to index and minor style to h1
    Work almost done on script

---
+++ script.js
@@ -0,0 +1,77 @@
+//const playerSelection = prompt("What's your weapon?","ROCK? PAPER? SCISSORS?").toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
+let roundScore = 0;
+
+
+
+function randomNumber() {
+    return Math.floor((Math.random() * 3) + 1);
+}
+
+
+
+function computerSelection() {
+let random = randomNumber();
+    if(random === 1) {
+        return "rock"
+    }
+    else if (random === 2) {
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+}
+
+
+function playRound(player, computer) {
+    console.log(player,computer)
+    if(player === computer) {
+        alert("Draw!");
+        return "Draw!"
+    }
+    else if(player === "rock" && computer === "scissors" || 
+    player === "paper" && computer === "rock" ||
+    player === "scissors" && computer === "paper") {
+        alert("You Win!");
+        return "You Win!"
+    }
+    else if(player === "rock" && computer === "paper" || 
+    player === "paper" && computer === "scissors" ||
+    player === "scissors" && computer === "rock") {
+        alert("You Lose!");
+        return "You Lose!"
+    }
+}
+
+function showScore() {
+    alert(`Round: ${roundScore} Player score: ${playerScore} Computer score: ${computerScore}`)
+}
+
+
+
+function keepScore(score) {
+    if (score === "Draw!") {
+        showScore();
+    }
+    else if (score === "You Win!") {
+        roundScore++;
+        showScore();
+    }
+    else if (score === "You Lose!") {
+        roundScore++;
+        showScore();
+    }
+    //alert(`round score:${roundScore}` ` ` `player score:${playerScore}` ` ` `computer score:${computerScore}`)
+}
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+       let playerSelection = prompt("What's your weapon?","ROCK? PAPER? SCISSORS?").toLowerCase();
+       keepScore(playRound(playerSelection, computerSelection()));
+     }
+}
+
+game();
